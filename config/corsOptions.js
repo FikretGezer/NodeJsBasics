525b55b5824312c974b76505e91bb922952d76ea
Mahmut Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2023-12-19 11:00:22 +0300

    JWT Auth Completed

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,14 +1,11 @@
 // cross origin resource sharing
-const whiteList = [
-    'https://www.google.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500'
-];
+const allowedOrigins = require('./allowedOrigins')
+
 
 const corsOptions = {
     origin: (origin, callback) => {
         console.log(origin);
-        if(whiteList.indexOf(origin) !== -1 || !origin){
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
             callback(null, true);
         } else {
             callback(new Error('Not allowed by cors'));
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const corsOptions = require('./config/corsOptions');
 const {logEvents, logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const verifyJTW = require('./middleware/verifyJWT');
+const credentials = require('./middleware/credentials');
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT || 3500;
 
@@ -13,8 +15,12 @@ const PORT = process.env.PORT || 3500;
 // custom middleware logger
 app.use(logger);
 
-app.use(cors(corsOptions));
+// Handle options credentials check - before CORS
+// and fetch cookies credentials requirement
+app.use(credentials);
 
+//Cross Origin Resource Sharing
+app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded data
 // in other words, form data:
@@ -24,12 +30,17 @@ app.use(express.urlencoded({ extended: true }))
 // built-in middleware for json
 app.use(express.json());
 
+// middleware for cookies
+app.use(cookieParser());
+
 //serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
 
 app.use(verifyJTW);
 app.use('/employees', require('./routes/api/employees'));
